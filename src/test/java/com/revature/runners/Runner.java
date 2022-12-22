106c135a818f5b79ef83aed5a26244f663a49f63
smcqueen186 <mcqueen.ston@gmail.com>
2022-12-22 10:34:19 -0500

    merge

--- src/test/java/com/revature/runners/Runner.java
+++ src/test/java/com/revature/runners/Runner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features", glue = "com.revature.stepimplementations", publish = true)
+
 public class Runner {}
 
 
